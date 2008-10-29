36555e94e0c91debbae4b7c31d676770c1b1c22e
ianb <devnull@localhost>
2008-10-29 02:47:12 +0000

    update news for release

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,7 @@ copyright = '2008, Ian Bicking'
 # The short X.Y version.
 version = '1.0'
 # The full version, including alpha/beta/rc tags.
-release = '1.0.1'
+release = '1.0.2'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
