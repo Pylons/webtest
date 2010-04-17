92a8c6b34061aa87989b1804d6dd3b07d23eadf1
Mike Korobov <kmike84@gmail.com>
2010-04-17 06:17:29 +0600

    Tests for existing behaviour regarding unicode support

--- tests/test_click.py
+++ tests/test_click.py
@@ -17,9 +17,8 @@ def links_app(environ, start_response):
                     <a href='baz/' id='id_baz'>Baz</a>
                     <a href='#' id='fake_baz'>Baz</a>
                     <a href='javascript:alert("123")' id='js_baz'>Baz</a>
-
                     <script>
-                        var link = "<a href='/boo/'>Foo</a>";
+                        var link = "<a href='/boo/'>Boo</a>";
                     </script>
                     <a href='/spam/'>Click me!</a>
                     <a href='/egg/'>Click me!</a>
@@ -33,11 +32,31 @@ def links_app(environ, start_response):
        '/baz/': '<html><body>This is baz.</body></html>',
        '/spam/': '<html><body>This is spam.</body></html>',
        '/egg/': '<html><body>Just eggs.</body></html>',
+
+       '/utf8/': u"""
+            <html>
+                <head><title>Тестовая страница</title></head>
+                <body>
+                    <a href='/foo/'>Менделеев</a>
+                    <a href='/baz/' title='Поэт'>Пушкин</a>
+                    <img src='/egg/' title='Поэт'>
+                    <script>
+                        var link = "<a href='/boo/'>Злодейская ссылка</a>";
+                    </script>
+                </body>
+            </html>
+            """.encode('utf8'),
     }
+
+    utf8_paths = ['/utf8/']
     body = responses[req.path_info]
     headers = [
         ('Content-Type', 'text/html'),
-        ('Content-Length', str(len(body)))]
+        ('Content-Length', str(len(body)))
+    ]
+    if req.path_info in utf8_paths:
+        headers[0] = ('Content-Type', 'text/html; charset=utf-8')
+
     start_response(status, headers)
     return [body]
 
@@ -47,7 +66,7 @@ def test_click():
     assert 'This is foo.' in app.get('/').click('Foo')
     assert 'This is foobar.' in app.get('/').click('Foo').click('Bar')
     assert 'This is bar.' in app.get('/').click('Bar')
-    assert 'This is baz.' in app.get('/').click('Baz')
+    assert 'This is baz.' in app.get('/').click('Baz') # should skip non-clickable links
     assert 'This is baz.' in app.get('/').click(linkid='id_baz')
     assert 'This is baz.' in app.get('/').click(href='baz/')
     assert 'This is baz.' in app.get('/').click(anchor="<a href='baz/' id='id_baz'>Baz</a>")
@@ -65,6 +84,33 @@ def test_click():
     def no_links_found():
         app.get('/').click('Ham')
     raises(IndexError, no_links_found)
+
+    def tag_inside_script():
+        app.get('/').click('Boo')
+    raises(IndexError, tag_inside_script)
+
+
+def test_click_utf8():
+    app = webtest.TestApp(links_app)
+    resp = app.get('/utf8/')
+    assert resp.charset == 'utf-8'
+    assert u"Тестовая страница".encode('utf8') in resp
+    assert u"Тестовая страница" in resp
+    assert 'This is foo.' in resp.click(u'Менделеев'.encode('utf8'))
+
+    # should skip the img tag
+    anchor_re = u".*title='Поэт'.*".encode('utf8')
+    assert 'This is baz.' in resp.click(anchor=anchor_re)
+
+
+#def test_click_unicode():
+#    app = webtest.TestApp(links_app)
+#    resp = app.get('/utf8/')
+#
+#    assert u"Тестовая страница" in resp
+#    assert 'This is foo.' in resp.click(u'Менделеев')
+#    assert 'This is baz.' in resp.click(anchor=u".*title='Поэт'.*")
+
 
 
 def test_parse_attrs():
