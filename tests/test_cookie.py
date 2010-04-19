ebb5ec77e441b50bd93c3466de5af762357656e6
Mike Korobov <kmike84@gmail.com>
2010-04-19 22:25:16 +0600

    Cookie tests for better test coverage

---
+++ tests/test_cookie.py
@@ -0,0 +1,34 @@
+import webtest
+from webob import Request
+
+def cookie_app(environ, start_response):
+    req = Request(environ)
+    status = '200 OK'
+    body = '<html><body><a href="/go/">go</a></body></html>'
+    headers = [
+        ('Content-Type', 'text/html'),
+        ('Content-Length', str(len(body))),
+    ]
+    if req.path_info != '/go/':
+        headers.extend([
+            ('Set-Cookie', 'spam=eggs'),
+            ('Set-Cookie', 'foo="bar;baz"'),
+        ])
+    start_response(status, headers)
+    return [body]
+
+def test_cookies():
+    app = webtest.TestApp(cookie_app)
+    assert not app.cookies, 'App should initially contain no cookies'
+    res = app.get('/')
+    cookies = app.cookies
+    assert cookies, 'Response should have set cookies'
+    assert cookies['spam'] == 'eggs'
+    assert cookies['foo'] == '"bar;baz"'
+
+def test_preserve_cookies():
+    app = webtest.TestApp(cookie_app)
+    res = app.get('/')
+    assert app.cookies
+    go_page = res.click('go')
+    assert app.cookies
