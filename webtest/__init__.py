723aa64ab5682be4f21410c221565b3ec9586f3b
Gael Pasgrimaud <gael@gawel.org>
2011-08-22 02:37:09 +0200

    pep8

--- webtest/__init__.py
+++ webtest/__init__.py
@@ -1,5 +1,7 @@
-# (c) 2005 Ian Bicking and contributors; written for Paste (http://pythonpaste.org)
-# Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
+# (c) 2005 Ian Bicking and contributors; written for Paste
+# (http://pythonpaste.org)
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/mit-license.php
 """
 Routines for testing WSGI applications.
 
