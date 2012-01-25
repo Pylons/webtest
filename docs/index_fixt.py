c87852d70516e607ca89aa2fc31f984c4531cc40
Gael Pasgrimaud <gael@gawel.org>
2012-01-25 09:52:39 +0100

    fix #30 bad link to pyquery documentation

--- docs/index_fixt.py
+++ docs/index_fixt.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from doctest import ELLIPSIS
 
+
 def setup_test(test):
+    raise ValueError(test.globs)
     for example in test.examples:
         example.options.setdefault(ELLIPSIS, 1)
-setup_test.__test__ = False
 
+setup_test.__test__ = False
