165c75894a698efa15f12d0446864c9437cfab46
Domen Kožar <domen@dev.si>
2012-11-14 20:36:43 +0100

    py3k compatible docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -40,8 +40,8 @@ source_suffix = '.txt'
 master_doc = 'index'
 
 # General information about the project.
-project = u'WebTest'
-copyright = u'2012, Ian Bicking'
+project = 'WebTest'
+copyright = '2012, Ian Bicking'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -183,8 +183,8 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'WebTest.tex', u'WebTest Documentation',
-   u'Ian Bicking', 'manual'),
+  ('index', 'WebTest.tex', 'WebTest Documentation',
+   'Ian Bicking', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -213,8 +213,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'webtest', u'WebTest Documentation',
-     [u'Ian Bicking'], 1)
+    ('index', 'webtest', 'WebTest Documentation',
+     ['Ian Bicking'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -227,8 +227,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'WebTest', u'WebTest Documentation',
-   u'Ian Bicking', 'WebTest', 'One line description of project.',
+  ('index', 'WebTest', 'WebTest Documentation',
+   'Ian Bicking', 'WebTest', 'One line description of project.',
    'Miscellaneous'),
 ]
 
