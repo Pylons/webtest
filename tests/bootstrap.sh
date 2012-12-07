bc632293bfeeea38a82a5b4dac39e209ead54032
Gael Pasgrimaud <gael@gawel.org>
2012-12-07 01:37:58 +0100

    make file executable

--- tests/bootstrap.sh
+++ tests/bootstrap.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-python -c "import os; print(os.uname())"
 
 if ! [ -x bin/casperjs ]; then
     python2.7 bootstrap.py -d
     bin/buildout install casperjs
 fi
+
+chmod +x parts/casperjs/*/bin/*js
+
+python -c "import os; print(os.uname())"
