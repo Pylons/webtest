fd6e6b3aeb355fa0a2f644201ed9a6685fb826ce
Gael Pasgrimaud <gael@gawel.org>
2012-12-17 22:59:39 +0100

    fix wrong paths

--- docs/index_fixt.py
+++ docs/index_fixt.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
-from doctest import ELLIPSIS
+import doctest
 
 
 def setup_test(test):
     for example in test.examples:
-        example.options.setdefault(ELLIPSIS, 1)
+        example.options.setdefault(doctest.ELLIPSIS, 1)
+        example.options.setdefault(doctest.NORMALIZE_WHITESPACE, 1)
 
 setup_test.__test__ = False
