a6db1a172d87656ce87041b02003287c9251c776
Gael Pasgrimaud <gael@gawel.org>
2013-01-29 12:35:01 +0100

    add some fixtures for quick start

--- docs/index_fixt.py
+++ docs/index_fixt.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
 import doctest
+import os
 
 
 def setup_test(test):
+    fd = open(os.path.join(os.path.dirname(__file__), 'form.html'), 'rb')
+    body = fd.read()
+    fd.close()
+    test.globs.update(body=body)
     for example in test.examples:
         example.options.setdefault(doctest.ELLIPSIS, 1)
         example.options.setdefault(doctest.NORMALIZE_WHITESPACE, 1)
