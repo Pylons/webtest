87edaf8aee5d0803f36666b109891d7c1a0aaeff
Gael Pasgrimaud <gael@gawel.org>
2013-02-21 23:35:01 +0100

    use bytes; pep8

--- tests/test_lint.py
+++ tests/test_lint.py
@@ -5,8 +5,6 @@ from tests.compat import unittest
 
 import webtest
 
-from six import PY3
-
 from webob import Request, Response
 
 
@@ -22,8 +20,9 @@ def application(environ, start_response):
         resp.body = env_input.readline(len_body)
     elif req.path_info == '/read_lines':
         resp.body = b'-'.join(env_input.readlines(len_body))
-        
+
     return resp(environ, start_response)
+
 
 def no_content_with_content(environ, start_response):
     status = "204 No Content"
@@ -34,6 +33,7 @@ def no_content_with_content(environ, start_response):
     start_response(status, headers)
     return [body]
 
+
 def no_content_type(environ, start_response):
     status = "200 OK"
     body = 'truc'
@@ -43,7 +43,6 @@ def no_content_type(environ, start_response):
     start_response(status, headers)
     return [body]
 
-   
 
 class TestForms(unittest.TestCase):
     def test_no_content(self):
@@ -54,19 +53,19 @@ class TestForms(unittest.TestCase):
         app = webtest.TestApp(no_content_type)
         self.assertRaises(AssertionError, app.get, '/')
 
+
 class TestInputWrapper(unittest.TestCase):
     def test_read(self):
         app = webtest.TestApp(application)
         resp = app.post('/read', 'hello')
-        self.assertEqual(resp.body, 'hello')
+        self.assertEqual(resp.body, b'hello')
 
     def test_readline(self):
         app = webtest.TestApp(application)
         resp = app.post('/read_line', 'hello\n')
-        self.assertEqual(resp.body, 'hello\n')
-
+        self.assertEqual(resp.body, b'hello\n')
 
     def test_readlines(self):
         app = webtest.TestApp(application)
         resp = app.post('/read_lines', 'hello\nt\n')
-        self.assertEqual(resp.body, 'hello\n-t\n')
+        self.assertEqual(resp.body, b'hello\n-t\n')
