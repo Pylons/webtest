7986de0a27488e489eae8ec47319dc6672fd8bdb
Arthur Vuillard <arthur.vuillard@gmail.com>
2013-02-21 15:35:30 +0100

    Little cleaning in test_forms.py

--- tests/test_forms.py
+++ tests/test_forms.py
@@ -5,26 +5,7 @@ from tests.compat import unittest
 import webtest
 from webtest.forms import NoValue
 
-class TestForms(unittest.TestCase):
-    def test_set_submit_field(self):
-        form = webtest.Form(None, '''
-<html>
-    <head><title>Page without form</title></head>
-    <body>
-        <form method="POST" id="second_form">
-            <input type="submit" name="submit" />
-        </form>
-    </body>
-</html>
-''')
-        self.assertRaises(
-            AttributeError,
-            form['submit'].value__set,
-            'foo'
-        )
-
-    def test_force_select(self):
-        form = webtest.Form(None, '''
+PAGE_CONTENT = '''
 <html>
     <head><title>Page without form</title></head>
     <body>
@@ -34,15 +15,26 @@ class TestForms(unittest.TestCase):
                 <option value="value2" selected>Value 2</option>
                 <option value="value3">Value 3</option>
             </select>       
+            <input type="submit" name="submit" />
         </form>
     </body>
 </html>
-''')
+'''
+
+class TestForms(unittest.TestCase):
+    def test_set_submit_field(self):
+        form = webtest.Form(None, PAGE_CONTENT)
+        self.assertRaises(
+            AttributeError,
+            form['submit'].value__set,
+            'foo'
+        )
+
+    def test_force_select(self):
+        form = webtest.Form(None, PAGE_CONTENT)
         form['select'].force_value('notavalue')
         form['select'].value__set('value3')
 
         assert form['select']._forced_value is NoValue
         assert form['select'].value == 'value3'
         assert form['select'].selectedIndex == 2
-
-
