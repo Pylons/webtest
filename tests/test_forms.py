e127c319bc59ac11c3177f37df6240ed57499864
Arthur Vuillard <arthur.vuillard@gmail.com>
2013-02-21 13:50:08 +0100

    Add a test on forcing (and unforcing) value with select field

--- tests/test_forms.py
+++ tests/test_forms.py
@@ -3,6 +3,7 @@
 from compat import unittest
 
 import webtest
+from webtest.forms import NoValue
 
 class TestForms(unittest.TestCase):
     def test_set_submit_field(self):
@@ -21,4 +22,27 @@ class TestForms(unittest.TestCase):
             form['submit'].value__set,
             'foo'
         )
+
+    def test_force_select(self):
+        form = webtest.Form(None, '''
+<html>
+    <head><title>Page without form</title></head>
+    <body>
+        <form method="POST" id="second_form">
+            <select name="select">
+                <option value="value1">Value 1</option> 
+                <option value="value2" selected>Value 2</option>
+                <option value="value3">Value 3</option>
+            </select>       
+        </form>
+    </body>
+</html>
+''')
+        form['select'].force_value('notavalue')
+        form['select'].value__set('value3')
+
+        assert form['select']._forced_value is NoValue
+        assert form['select'].value == 'value3'
+        assert form['select'].selectedIndex == 2
+
 
