0cabca0c24dd3b56a4d8b1f912bf78f238fd655c
Domen Kožar <domen@dev.si>
2013-08-07 14:16:24 +0200

    Back to development: 2.0.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 from setuptools import setup
 from setuptools import find_packages
 
-version = '2.0.7'
+version = '2.0.8.dev0'
 
 tests_require = ['nose', 'coverage', 'mock', 'PasteDeploy', 'WSGIProxy2', 'pyquery']
 opt_install_requires = []
