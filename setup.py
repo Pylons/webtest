515ce8e74f47ec737b19a36a88b26cf7d67927a4
Gael Pasgrimaud <gael@gawel.org>
2014-04-15 21:02:34 +0200

    small cleaning / pep8

--- setup.py
+++ setup.py
@@ -7,11 +7,20 @@ from setuptools import find_packages
 
 version = '2.0.15.dev0'
 
-tests_require = ['nose', 'coverage', 'mock', 'PasteDeploy', 'WSGIProxy2', 'pyquery']
-opt_install_requires = []
+install_requires = [
+    'six',
+    'WebOb>=1.2',
+    'waitress>=0.8.5',
+    'beautifulsoup4',
+]
+
+tests_require = [
+    'nose', 'coverage', 'mock',
+    'PasteDeploy', 'WSGIProxy2', 'pyquery'
+]
 
 if sys.version_info[0:2] < (2, 7):
-    opt_install_requires.append('ordereddict')
+    install_requires.append('ordereddict')
     tests_require.append('unittest2')
 
 
@@ -20,18 +29,18 @@ setup(name='WebTest',
       description="Helper to test WSGI applications",
       long_description=open('README.rst').read(),
       classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Framework :: Paste",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
-        "Topic :: Internet :: WWW/HTTP :: WSGI",
-        "Topic :: Internet :: WWW/HTTP :: WSGI :: Server",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.6",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.2",
-        "Programming Language :: Python :: 3.3",
+          "Development Status :: 5 - Production/Stable",
+          "Framework :: Paste",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: MIT License",
+          "Topic :: Internet :: WWW/HTTP :: WSGI",
+          "Topic :: Internet :: WWW/HTTP :: WSGI :: Server",
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 2.6",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.2",
+          "Programming Language :: Python :: 3.3",
       ],
       keywords='wsgi test unit tests web',
       author='Ian Bicking',
@@ -49,16 +58,11 @@ setup(name='WebTest',
       ]),
       include_package_data=True,
       zip_safe=False,
-      install_requires=[
-        'six',
-        'WebOb>=1.2',
-        'waitress>=0.8.5',
-        'beautifulsoup4',
-      ] + opt_install_requires,
+      install_requires=install_requires,
       test_suite='nose.collector',
       tests_require=tests_require,
       extras_require={
-            'tests': tests_require,
+          'tests': tests_require,
       },
       entry_points="""
       [paste.app_factory]
