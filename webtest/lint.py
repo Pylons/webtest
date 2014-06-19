130c55cf3b71bcef1b346edfad12bb7f044d219b
Victor Lin <bornstub@gmail.com>
2014-06-19 11:15:41 -0700

    Only check header unicode lint in Python 2

--- webtest/lint.py
+++ webtest/lint.py
@@ -455,9 +455,10 @@ def check_headers(headers):
             "Bad header value: %r (bad char: %r)"
             % (str_value, bad_header_value_re.search(str_value).group(0)))
 
-        assert isinstance(value, binary_type), (
-            "Header %r has a non-%r object: %r" % (name, binary_type, value)
-        )
+        if not PY3:
+            assert isinstance(value, binary_type), (
+                "Header %r has a non-%r object: %r" % (name, binary_type, value)
+            )
 
 
 def check_content_type(status, headers):
