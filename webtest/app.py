1035510088f3a30bcd820c9f4404b93e356827c6
Gael Pasgrimaud <gael@gawel.org>
2015-05-04 22:37:21 +0200

    fixed #135

--- webtest/app.py
+++ webtest/app.py
@@ -99,7 +99,8 @@ class TestApp(object):
         .. versionadded:: 2.0
 
         It can also be an actual full URL to an http server and webtest
-        will proxy requests with `wsgiproxy`.
+        will proxy requests with `WSGIProxy2
+        <https://pypi.python.org/pypi/WSGIProxy2/0.4.2>`_.
     :type app:
         WSGI application
     :param extra_environ:
