8cb8cf769b5b502bcd31e3f40b8506df09125504
Pranay Suresh <psuresh@teslamotors.com>
2016-04-07 17:15:33 -0700

    support bearer auth

--- webtest/app.py
+++ webtest/app.py
@@ -204,6 +204,8 @@ class TestApp(object):
                     val = ':'.join(list(val))
                     val = b64encode(to_bytes(val)).strip()
                     val = val.decode('latin1')
+                elif authtype == 'Bearer' and val and isinstance(val, (str, unicode)):
+                    val = val.strip()
                 else:
                     raise ValueError(invalid_value)
                 value = str('%s %s' % (authtype, val))
