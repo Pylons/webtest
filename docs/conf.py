7f360d4d7cd2342c8b833ec3d14f3b8801da97d1
Steve Piercy <web@stevepiercy.com>
2018-09-10 23:45:43 -0700

    PEP8

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,11 @@ import pylons_sphinx_themes
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.intersphinx']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.viewcode',
+]
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
