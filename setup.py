66482ed67b812ccacac0264141e452baaf72b961
Michael Howitz <mh@gocept.com>
2020-11-10 09:38:57 +0100

    Clean-up after dropping Python 2 support.
    These are the things I'd suggest to do additionally when dropping Python 2
    support. They ensure that users still using Python 2 do not accidentally
    install this new version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 from setuptools import find_packages
 
-version = '2.0.36.dev0'
+version = '3.0.0.dev0'
 
 install_requires = [
     'WebOb>=1.2',
@@ -34,6 +34,7 @@ setup(name='WebTest',
           "Topic :: Internet :: WWW/HTTP :: WSGI",
           "Topic :: Internet :: WWW/HTTP :: WSGI :: Server",
           "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3 :: Only",
           "Programming Language :: Python :: 3.6",
           "Programming Language :: Python :: 3.7",
           "Programming Language :: Python :: 3.8",
@@ -53,6 +54,7 @@ setup(name='WebTest',
       ]),
       include_package_data=True,
       zip_safe=False,
+      python_requires='>=3.6, <4',
       install_requires=install_requires,
       test_suite='nose.collector',
       tests_require=tests_require,
