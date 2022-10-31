f1784563e69296bd9e8e72d77afac1be47af748c
Tim Gates <tim.gates@iress.com>
2022-11-01 06:57:12 +1100

    docs: Fix a few typos
    There are small typos in:
    - webtest/http.py
    - webtest/lint.py
    
    Fixes:
    - Should read `formatted` rather than `formated`.
    - Should read `compliance` rather than `compliancy`.
    
    Signed-off-by: Tim Gates <tim.gates@iress.com>

--- webtest/lint.py
+++ webtest/lint.py
@@ -152,7 +152,7 @@ def middleware(application, global_conf=None):
 
     """
     When applied between a WSGI server and a WSGI application, this
-    middleware will check for WSGI compliancy on a number of levels.
+    middleware will check for WSGI compliance on a number of levels.
     This middleware does not modify the request or response in any
     way, but will throw an AssertionError if anything seems off
     (except for a failure to close the application iterator, which
